cacfa2235fcea8c1cbdf495b05732630fad0fd9a
APToons <43712812+APToons@users.noreply.github.com>
2022-12-15 07:35:48 +0000

    SM template

---
+++ pages/SlotMachine.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+const SlotMachine = ({ numSlots, slotValues, spinTime }) => {
+  const [slots, setSlots] = useState(Array(numSlots).fill(slotValues[0]));
+
+  const spin = () => {
+    const newSlots = [];
+    for (let i = 0; i < numSlots; i++) {
+      const randomIndex = Math.floor(Math.random() * slotValues.length);
+      newSlots.push(slotValues[randomIndex]);
+    }
+    setSlots(newSlots);
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(spin, spinTime);
+    return () => clearTimeout(timeout);
+  }, [spinTime]);
+
+  return (
+    <div>
+      {slots.map((slot, index) => (
+        <div key={index}>{slot}</div>
+      ))}
+      <button onClick={spin}>Spin!</button>
+    </div>
+  );
+};
